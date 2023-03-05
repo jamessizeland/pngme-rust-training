6a9dddda3cef2030937b47b21685eeab7b5d255a
James Sizeland <jamessizeland@gmail.com>
2023-03-05 16:35:27 +0000

    chunk type unit tested

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # pngme-rust-training
-working from https://picklenerd.github.io/pngme_book/
+
+## Overview
+
+Working from <https://picklenerd.github.io/pngme_book/>
+
+Implementing the [PNG Spec](http://www.libpng.org/pub/png/spec/1.2/PNG-Structure.html)
---
+++ src/chunk_type.rs
@@ -0,0 +1,191 @@
+//! A 4-byte chunk type code. For convenience in description and in examining PNG files, type codes are restricted to consist of uppercase and lowercase ASCII letters (A-Z and a-z, or 65-90 and 97-122 decimal). However, encoders and decoders must treat the codes as fixed binary values, not character strings. For example, it would not be correct to represent the type code IDAT by the EBCDIC equivalents of those letters.
+
+use anyhow::{anyhow, Error};
+use std::str;
+
+/// Chunk type codes are assigned so that a decoder can determine some properties of a chunk even when it does not recognize the type code.
+///
+/// These rules are intended to allow safe, flexible extension of the PNG format, by allowing a decoder to decide what to do when it encounters an unknown chunk.
+///
+/// The naming rules are not normally of interest when the decoder does recognize the chunk's type.
+#[derive(PartialEq, Eq, Debug)]
+pub struct ChunkType {
+    raw: [u8; 4],
+}
+
+impl TryFrom<[u8; 4]> for ChunkType {
+    type Error = Error;
+
+    /// Check that all bytes are valid utf-8 and ASCII
+    fn try_from(value: [u8; 4]) -> std::result::Result<Self, Self::Error> {
+        match str::from_utf8(&value) {
+            Ok(_) => Ok(ChunkType { raw: value }),
+            Err(err) => Err(err.into()),
+        }
+    }
+}
+impl str::FromStr for ChunkType {
+    type Err = Error;
+
+    /// Check that string is valid ASCII and that there are at least 4 bytes to make a valid chunk with
+    fn from_str(s: &str) -> std::result::Result<Self, Self::Err> {
+        let bytes: [u8; 4] = s.as_bytes().try_into()?;
+        if bytes.iter().fold(true, |valid, byte| {
+            (byte.is_ascii_lowercase() || byte.is_ascii_uppercase()) && valid
+        }) {
+            return Ok(ChunkType { raw: bytes });
+        }
+        Err(anyhow!("chunk type: {:?} is invalid", bytes))
+    }
+}
+impl std::fmt::Display for ChunkType {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match str::from_utf8(&self.bytes()) {
+            Ok(s) => write!(f, "{s}"),
+            Err(_) => write!(f, "invalid"),
+        }
+    }
+}
+impl ChunkType {
+    /// Get the raw bytes of this chunk type
+    fn bytes(&self) -> [u8; 4] {
+        self.raw
+    }
+    /// Check if chunk is valid ASCII and valid PNG
+    fn is_valid(&self) -> bool {
+        self.bytes().iter().fold(true, |valid, byte| {
+            (byte.is_ascii_lowercase() || byte.is_ascii_uppercase()) && valid
+        }) && self.is_reserved_bit_valid()
+    }
+    /// 0 (uppercase) = critical, 1 (lowercase) = ancillary.
+    ///
+    /// Chunks that are not strictly necessary in order to meaningfully display the contents of the file are known as "ancillary" chunks. A decoder encountering an unknown chunk in which the ancillary bit is 1 can safely ignore the chunk and proceed to display the image. The time chunk (tIME) is an example of an ancillary chunk.
+    ///
+    /// Chunks that are necessary for successful display of the file's contents are called "critical" chunks. A decoder encountering an unknown chunk in which the ancillary bit is 0 must indicate to the user that the image contains information it cannot safely interpret. The image header chunk (IHDR) is an example of a critical chunk.
+    fn is_critical(&self) -> bool {
+        let byte = self.bytes()[0];
+        byte >> 5 & 1 == 0
+    }
+    /// 0 (uppercase) = public, 1 (lowercase) = private.
+    ///
+    /// A public chunk is one that is part of the PNG specification or is registered in the list of PNG special-purpose public chunk types. Applications can also define private (unregistered) chunks for their own purposes. The names of private chunks must have a lowercase second letter, while public chunks will always be assigned names with uppercase second letters. Note that decoders do not need to test the private-chunk property bit, since it has no functional significance; it is simply an administrative convenience to ensure that public and private chunk names will not conflict.
+    fn is_public(&self) -> bool {
+        let byte = self.bytes()[1];
+        byte >> 5 & 1 == 0
+    }
+    /// Must be 0 (uppercase) in files conforming to this version of PNG.
+    ///
+    /// The significance of the case of the third letter of the chunk name is reserved for possible future expansion. At the present time all chunk names must have uppercase third letters.
+    ///
+    /// (Decoders should not complain about a lowercase third letter, however, as some future version of the PNG specification could define a meaning for this bit. It is sufficient to treat a chunk with a lowercase third letter in the same way as any other unknown chunk type.)
+    fn is_reserved_bit_valid(&self) -> bool {
+        let byte = self.bytes()[2];
+        byte >> 5 & 1 == 0
+    }
+    /// 0 (uppercase) = unsafe to copy, 1 (lowercase) = safe to copy.
+    ///
+    /// This property bit is not of interest to pure decoders, but it is needed by PNG editors (programs that modify PNG files). This bit defines the proper handling of unrecognized chunks in a file that is being modified.
+    fn is_safe_to_copy(&self) -> bool {
+        let byte = self.bytes()[3];
+        byte >> 5 & 1 == 1
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::convert::TryFrom;
+    use std::str::FromStr;
+
+    #[test]
+    pub fn test_chunk_type_from_bytes() {
+        let expected = [82, 117, 83, 116];
+        let actual = ChunkType::try_from([82, 117, 83, 116]).unwrap();
+
+        assert_eq!(expected, actual.bytes());
+    }
+
+    #[test]
+    pub fn test_chunk_type_from_str() {
+        let expected = ChunkType::try_from([82, 117, 83, 116]).unwrap();
+        let actual = ChunkType::from_str("RuSt").unwrap();
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    pub fn test_chunk_type_is_critical() {
+        let chunk = ChunkType::from_str("RuSt").unwrap();
+        assert!(chunk.is_critical());
+    }
+
+    #[test]
+    pub fn test_chunk_type_is_not_critical() {
+        let chunk = ChunkType::from_str("ruSt").unwrap();
+        assert!(!chunk.is_critical());
+    }
+
+    #[test]
+    pub fn test_chunk_type_is_public() {
+        let chunk = ChunkType::from_str("RUSt").unwrap();
+        assert!(chunk.is_public());
+    }
+
+    #[test]
+    pub fn test_chunk_type_is_not_public() {
+        let chunk = ChunkType::from_str("RuSt").unwrap();
+        assert!(!chunk.is_public());
+    }
+
+    #[test]
+    pub fn test_chunk_type_is_reserved_bit_valid() {
+        let chunk = ChunkType::from_str("RuSt").unwrap();
+        assert!(chunk.is_reserved_bit_valid());
+    }
+
+    #[test]
+    pub fn test_chunk_type_is_reserved_bit_invalid() {
+        let chunk = ChunkType::from_str("Rust").unwrap();
+        println!("reserved bit: {}", chunk.is_reserved_bit_valid());
+        assert!(!chunk.is_reserved_bit_valid());
+    }
+
+    #[test]
+    pub fn test_chunk_type_is_safe_to_copy() {
+        let chunk = ChunkType::from_str("RuSt").unwrap();
+        assert!(chunk.is_safe_to_copy());
+    }
+
+    #[test]
+    pub fn test_chunk_type_is_unsafe_to_copy() {
+        let chunk = ChunkType::from_str("RuST").unwrap();
+        assert!(!chunk.is_safe_to_copy());
+    }
+
+    #[test]
+    pub fn test_valid_chunk_is_valid() {
+        let chunk = ChunkType::from_str("RuSt").unwrap();
+        assert!(chunk.is_valid());
+    }
+    #[test]
+    pub fn test_invalid_chunk_is_valid() {
+        let chunk = ChunkType::from_str("Rust").unwrap();
+        assert!(!chunk.is_valid(), "valid");
+
+        let chunk = ChunkType::from_str("Ru1t");
+        assert!(chunk.is_err(), "error");
+    }
+
+    #[test]
+    pub fn test_chunk_type_string() {
+        let chunk = ChunkType::from_str("RuSt").unwrap();
+        assert_eq!(&chunk.to_string(), "RuSt");
+    }
+
+    #[test]
+    pub fn test_chunk_type_trait_impls() {
+        let chunk_type_1: ChunkType = TryFrom::try_from([82, 117, 83, 116]).unwrap();
+        let chunk_type_2: ChunkType = FromStr::from_str("RuSt").unwrap();
+        let _chunk_string = format!("{}", chunk_type_1);
+        let _are_chunks_equal = chunk_type_1 == chunk_type_2;
+    }
+}
